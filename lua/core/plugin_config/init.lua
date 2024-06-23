14ec3e28c72304b1b1ad6d3e04be50e8d232c428
DrazenG <drazengordon@gmail.com>
2024-06-24 00:33:47 +0100

    init commit nvim

---
+++ lua/core/plugin_config/init.lua
@@ -0,0 +1,5 @@
+require("core.plugin_config.theme")
+require("core.plugin_config.harpoon")
+require("core.plugin_config.telescope")
+require("core.plugin_config.treesitter")
+require("core.plugin_config.lsp")
