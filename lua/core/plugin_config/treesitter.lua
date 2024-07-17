9774abf268760fe9620461183b0c8bef04c4be23
Dražen Elvis Gordon <62464834+Drazbaz@users.noreply.github.com>
2024-07-17 16:28:12 +0100

    Update treesitter.lua

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c_sharp", "svelte", "json", "javascript", "typescript", "lua", "vim" },
+  ensure_installed = { "c_sharp", "svelte", "json", "javascript", "typescript", "lua", "vim", "zig" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
